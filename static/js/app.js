f09b4f73436ab2cd4adecf5f3e7d8b675743c84b
mohamedusmaan17 <usmaanmohamed16@gmail.com>
2026-08-19 00:23:18 +0530

    Fix syntax error in app.js

--- static/js/app.js
+++ static/js/app.js
@@ -1060,7 +1060,7 @@ function openPaymentGatewayModal() {
         timeLeft--;
         const m = Math.floor(timeLeft / 60).toString().padStart(2, '0');
         const s = (timeLeft % 60).toString().padStart(2, '0');
-        if (timerEl) timerEl.innerText = \`⏱ Expires in \${m}:\${s}\`;
+        if (timerEl) timerEl.innerText = `⏱ Expires in ${m}:${s}`;
         
         if (timeLeft <= 0) {
             clearInterval(window.qrTimerInterval);
